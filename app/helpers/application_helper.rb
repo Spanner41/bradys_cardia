75250c3d458b1e0a8c94ad970bb1e053bbe48421
Brady Steed <Brady@Nicoles-MacBook-Pro.local>
2020-11-22 13:37:43 -0600

    feat: improve layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
     def navitems
         [
             { href: root_path, title: "Profile" },
-            { href: design_system_index_path, title: "Design System" },
             { href: portfolio_index_path, title: "Portfolio" },
+            { href: design_system_index_path, title: "Design System" },
             { href: contact_index_path, title: "Contact" }
         ]
     end
