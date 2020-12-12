bc5029dfc51e2b443b31a7d5212f3470c4d4bdd3
Brady Steed <Brady@Nicoles-MacBook-Pro.local>
2020-12-12 16:28:54 -0600

    feat: remove Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,5 +16,4 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import "bootstrap"
 import "../stylesheets/application"
